22c9d526b2361e6d5eaee7865d74ec2cf72017f1
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-12-05 19:36:45 +0530

    upto medium completed

---
+++ medium/palindrome.js
@@ -0,0 +1,12 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  if (cleanStr === cleanStr.split('').reverse().join('')){ return true; } 
+  else { return false; }
+}
+
+module.exports = isPalindrome;
